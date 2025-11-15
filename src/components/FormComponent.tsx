250222e4ac7955bc2f943406454763e22daccf1e
Luffy01012000 <goku05555@gmail.com>
2025-11-16 01:51:29 +0530

    initial commit

---
+++ src/components/FormComponent.tsx
@@ -0,0 +1,132 @@
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Select, SelectContent, SelectItem, SelectSeparator, SelectTrigger, SelectValue } from "./ui/select"
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  language: z
+    .string()
+    .min(1, "Please select your spoken language.")
+    .refine((val) => val !== "auto", {
+      message:
+        "Auto-detection is not allowed. Please select a specific language.",
+    }),
+})
+
+const spokenLanguages = [
+  { label: "English", value: "en" },
+  { label: "Spanish", value: "es" },
+  { label: "French", value: "fr" },
+  { label: "German", value: "de" },
+  { label: "Italian", value: "it" },
+  { label: "Chinese", value: "zh" },
+  { label: "Japanese", value: "ja" },
+] as const
+
+type TFormProps = {
+    initialData?: TSchemaVal & {id:string};
+}
+type TSchemaVal = z.infer<typeof formSchema>;
+const transformData = (data:TSchemaVal & {id:string}): TSchemaVal| undefined=>{
+    if(!data) return undefined;
+    console.log("data:",data)
+    return {
+        username: data.username ?? "",
+        language: data.language ?? "",
+    }
+}
+const FormComponent = ({initialData}: TFormProps) => {
+    console.log("initialData:",initialData);
+
+  const form = useForm<TSchemaVal>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData ? transformData(initialData): undefined,
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: TSchemaVal) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="language"
+          render={({ field, fieldState }) => (
+            <FormItem>
+              <FormLabel>Language</FormLabel>
+              <FormControl>
+                <Select
+                    name={field.name}
+                    // defaultValue={field.value}
+                    value={field.value}
+                    onValueChange={field.onChange}
+                  >
+                    <SelectTrigger
+                      id="form-rhf-select-language"
+                      aria-invalid={fieldState.invalid}
+                      className="min-w-[120px]"
+                    >
+                      <SelectValue placeholder="Select" />
+                    </SelectTrigger>
+                    <SelectContent position="item-aligned">
+                      <SelectItem value="auto">Auto</SelectItem>
+                      <SelectSeparator />
+                      {spokenLanguages.map((language) => (
+                        <SelectItem key={language.value} value={language.value}>
+                          {language.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+    
+  )
+}
+
+export default FormComponent
